add078b9f568fe31496864aa961ca1f675b55175
James Darling <james@abscond.org>
2016-07-14 12:49:21 +0100

    Make a route picker that reads DB

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,11 @@
 class Route < ActiveRecord::Base
   has_many :stops, -> { order(position: :asc) }
   has_many :journeys, -> { order(start_time: :asc) }
+
+  def self.bookable_routes
+    all.reject {|route| route.stops.count <= 2}
+  end
+
   def name
     if stops.count > 1
       "Route #{self.id}: #{self.stops.first.name} - #{self.stops.last.name}"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :routes
+
   resources :journeys do
     collection do
       get 'routes'
