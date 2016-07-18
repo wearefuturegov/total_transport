ea225c1b4a2412821dbb8ab2e5cb35fdd89d33b5
Chris Evans <evanschris@me.com>
2016-07-18 12:04:26 +0100

    Major update of styles & integration with style guide

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,22 +1,6 @@
 $(document).ready(function() {
   var numOfRoutes = $('.route').length;
 
-  $('.route .acordBtn').click(function() {
-    var parent = $(this).parent('.route');
-    if (parent.hasClass('open')) {
-      parent.removeClass('open');
-      parent.find('.acord').slideUp();
-    } else {
-      
-      $('.route').each(function(){
-        $(this).removeClass('open').find('.acord').slideUp();
-      }).promise().done( function(){
-        parent.find('.acord').slideDown();
-      });
-      parent.addClass('open');
-    }
-  });
-
   $('.input-cont input[name="from"]').keyup(function() {
     // On input change
     var tempTxt = $( this ).val();
