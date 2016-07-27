43966752ccc950610af24fe833da03b111a9ed5c
James Darling <james@abscond.org>
2016-07-27 11:40:14 +0100

    Add user and vehicle to a journey

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -2,6 +2,8 @@ class Journey < ActiveRecord::Base
   belongs_to :route
   has_many :stops, through: :route
   has_many :bookings, dependent: :destroy
+  belongs_to :vehicle
+  belongs_to :user
 
   scope :available, -> {where('start_time > ?', Time.now)}
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,4 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :team
+  has_many :journeys
 end
