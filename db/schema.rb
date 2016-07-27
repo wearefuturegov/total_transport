57d0837751cca21184e48047fe27ea99316f6da3
James Darling <james@abscond.org>
2016-07-27 14:36:48 +0100

    Add name and phone number to suppliers
    and
    * add validation for presence on both
    * update new and edit supplier forms
    * use name instead of email address when referring to supplier

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727104127) do
+ActiveRecord::Schema.define(version: 20160727132115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 20160727104127) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "team_id"
+    t.string   "name"
+    t.string   "phone_number"
   end
 
   add_index "suppliers", ["email"], name: "index_suppliers_on_email", unique: true, using: :btree
