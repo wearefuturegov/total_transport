e699ce59ad153bcd2b810255f139235ad8f21caa
James Darling <james@abscond.org>
2016-08-10 18:15:53 +0100

    Show friendly dates ("Today" etc) on choose journey page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,18 @@ module ApplicationHelper
     " - " +
     (time + 10.minutes).strftime("%H:%M")
   end
+
+  def friendly_date(date)
+    if date == Date.today
+      "Today"
+    elsif date == Date.yesterday
+      "Yesterday"
+    elsif (date > Date.today - 7) && (date < Date.yesterday)
+      date.strftime("%A")
+    elsif (date > Date.today.beginning_of_year) && (date < Date.today.end_of_year)
+      date.strftime("%B %-d")
+    else
+      date.strftime("%B %-d %Y")
+    end
+  end
 end
