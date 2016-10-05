bdee33fee2624ab3bc6350ac293c63874ca1e24b
Chris Evans <evanschris@me.com>
2016-10-05 15:04:47 +0100

    link 4 digit verify to backend

--- app/assets/javascripts/globals.js
+++ app/assets/javascripts/globals.js
@@ -113,15 +113,6 @@ $(document).ready(function() {
       .on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
     });
   }
-
-
-  $('.single-digit').keypress(function(event){
-    if(event.which == 8 && isNaN(String.fromCharCode(event.which))){
-      event.preventDefault(); 
-    } else {
-      $(this).next('input').focus();
-    }
-  });
 });
 
 
