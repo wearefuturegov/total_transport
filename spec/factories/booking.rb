426395c514169a653c1470d534ec4d114776b77d
James Darling <james@abscond.org>
2016-10-13 15:16:55 +0100

    fix booking factory

--- spec/factories/booking.rb
+++ spec/factories/booking.rb
@@ -2,14 +2,14 @@ FactoryGirl.define do
   factory(:booking) do
     dropoff_lat nil
     dropoff_lng nil
-    dropoff_stop
+    dropoff_stop factory: :stop
     journey
     passenger
     payment_method
     phone_number nil
     pickup_lat nil
     pickup_lng nil
-    pickup_stop
+    pickup_stop factory: :stop
     state nil
   end
 end
