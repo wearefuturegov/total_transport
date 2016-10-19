6b349029e2131b2754ebac69b5ae7fad0c634651
Chris Evans <evanschris@me.com>
2016-10-19 12:29:32 +0100

    fix bug with route accordian

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,7 +1,7 @@
 class RoutesController < ApplicationController
   def index
     @page_title = "Choose Your Route"
-    @top_sec = "These routes are flexible and show the general areas that your PickUp will pass through."
+    @top_sec = "These routes are flexible and show the general areas that your Pickup will pass through."
   end
 
   def suggest_route
