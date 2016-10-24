ba2f70211661e9e9d044230dcadcce2ffee0a28a
Chris Evans <evanschris@me.com>
2016-10-24 16:50:01 +0100

    fix basic filtering on routes page

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  var numOfRoutes = $('.route').length;
+  var numOfRoutes = $('.full-route').length;
 
   $('#filter').keyup(function() {
     var tempTxt = $( this ).val();
     if (tempTxt.length > 2) {
       checkRoutes(tempTxt);
     } else {
-      $('.route').each(function() {
+      $('.full-route').each(function() {
         // the text is 2 char or below, reveal any hidden routes 
         $(this).slideDown();
         $(this).removeClass('filtered');
@@ -16,24 +16,22 @@ $(document).ready(function() {
 
   function checkRoutes(tempTxt) {
     var exception = [];
-    $('.route .acord li').each(function() {
+    $('.full-route .non-reverse .acord li').each(function() {
       // for each list item
       if ($(this).html().toLowerCase().indexOf(tempTxt.toLowerCase()) >= 0) {
         // check if it contains the same text 
         // if so add to id array 
-        exception.push($(this).closest('.route').attr('id'));
-        
+        exception.push($(this).closest('.full-route').attr('id'));
       } else {
         // if not remove vis class
-        $(this).closest('.route').removeClass('filtered');
+        $(this).closest('.full-route').removeClass('filtered');
       }
     });
-    
     $(exception).each(function(index, item) {
       // after checking all that add the vis class to all that need it
       $('#'+item).addClass('filtered');
     });
-    $('.route').each(function() {
+    $('.full-route').each(function() {
       // run through all routes - if they dont have the vis class - kill them
       if(!$(this).hasClass('filtered')) {
         $(this).slideUp();
