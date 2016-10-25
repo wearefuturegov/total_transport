5c7fe0312de14c532494e4adebf8a7cc226c6221
James Darling <james@abscond.org>
2016-10-25 12:38:23 +0100

    Fix flash alerts
    Seeing as we’re not redirecting on these errors, set as instance
    variable rather than in flash, as that displays on the *next* request.

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -12,7 +12,7 @@ class PassengersController < ApplicationController
       redirect_to verify_passenger_path(id: @passenger.id)
     else
       @passenger = Passenger.new
-      flash[:alert] = "Phone number is not a valid, please try another one"
+      @flash_alert = "Phone number is not a valid, please try another one"
       render action: 'new'
     end
   end
@@ -30,7 +30,7 @@ class PassengersController < ApplicationController
         set_current_passenger(@passenger)
         redirect_to root_path
       else
-        flash[:alert] = "That verification code was incorrect. Please try again."
+        @flash_alert = "That verification code was incorrect. Please try again."
       end
     end
   end
