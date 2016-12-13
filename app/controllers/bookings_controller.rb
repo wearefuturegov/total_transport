60ea2de93ce5411e43c086626bd806c56e6581c4
Chris Evans <evanschris@me.com>
2016-12-13 10:34:09 +0000

    fix text message bug

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
   before_filter :find_route, except: [:show]
   before_filter :find_booking, except: [:new, :create]
+  include ApplicationHelper
 
   # Choose stops
   def new
@@ -137,7 +138,7 @@ class BookingsController < ApplicationController
     @booking.update_attributes(booking_params)
     @passenger = current_passenger
     @passenger.update_attributes(:name => @booking[:passenger_name])
-    @booking.send_notification!("Your Pickup boking from #{@passenger.bookings.last.pickup_stop.name} to #{@passenger.bookings.last.dropoff_stop.name} is confirmed. Your vehicle will pick you up from #{@passenger.pickup_name} on <%= friendly_date @booking.journey.start_time %> between #{plus_minus_ten @booking.pickup_stop.time_for_journey(@booking.journey)}. You can review or cancel your booking here: #{passenger_booking_url(@passenger.bookings.last)}")
+    @booking.send_notification!("Your Pickup boking from #{@passenger.bookings.last.pickup_stop.name} to #{@passenger.bookings.last.dropoff_stop.name} is confirmed. Your vehicle will pick you up from #{@booking.pickup_name} on #{friendly_date @booking.journey.start_time} between #{plus_minus_ten(@booking.pickup_stop.time_for_journey(@booking.journey))}. You can review or cancel your booking here: #{passenger_booking_url(@passenger.bookings.last)}")
     redirect_to confirmation_route_booking_path(@route, @booking)
   end
 
