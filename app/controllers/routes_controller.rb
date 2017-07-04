60e4bd2d3963a124b168f6a86f9fb31f40199975
pezholio <pezholio@gmail.com>
2017-07-04 14:40:46 +0100

    Add specs for RoutesController

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,7 @@
 class RoutesController < ApplicationController
   def index
     @page_title = "Choose Your Route"
+    @routes = Route.bookable_routes
   end
 
   def suggest_route
--- spec/factories/route.rb
+++ spec/factories/route.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory(:route) do
-
+    transient { stops_count 5 }
+    after(:create) do |route, evaluator|
+      create_list(:stop, evaluator.stops_count, route: route)
+    end
   end
 end
