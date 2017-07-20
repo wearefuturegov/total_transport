150da92d239598a58d17442324c8cfbf2a680703
pezholio <pezholio@gmail.com>
2017-07-20 15:40:56 +0100

    Add Promo Code spec

--- spec/models/promo_code_spec.rb
+++ spec/models/promo_code_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe PromoCode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:promo_code) { FactoryGirl.create(:promo_code) }
+  
+  it 'creates a code' do
+    expect(SecretSanta).to receive(:create_code) { 'sekritc0d3' }
+    promo_code = FactoryGirl.create(:promo_code)
+    expect(promo_code.code).to eq('sekritc0d3')
+  end
+  
+  it 'finds a promo code by code' do
+    code = promo_code.code
+    expect(PromoCode.find_by_code(code)).to eq(promo_code)
+  end
+  
+  context '#used?' do
+    
+    it 'returns true if a booking is present' do
+      promo_code.booking = FactoryGirl.create(:booking)
+      expect(promo_code.used?).to eq(true)
+    end
+    
+    it 'returns false if a booking is not present' do
+      expect(promo_code.used?).to eq(false)
+    end
+    
+  end
+  
+  context '#available?' do
+    
+    it 'returns true if a booking is not present' do
+      expect(promo_code.available?).to eq(true)
+    end
+    
+    it 'returns false if a booking is present' do
+      promo_code.booking = FactoryGirl.create(:booking)
+      expect(promo_code.available?).to eq(false)
+    end
+    
+  end
+  
 end
