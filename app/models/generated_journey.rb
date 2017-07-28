7773f8e5ea3aed2c6de97be2b5809503fe1e5edd
pezholio <pezholio@gmail.com>
2017-07-28 14:43:19 +0100

    Add generated_journey views

--- app/models/generated_journey.rb
+++ app/models/generated_journey.rb
@@ -4,6 +4,15 @@ class GeneratedJourney < ActiveRecord::Base
   has_and_belongs_to_many :vehicles
   has_many :teams, through: :vehicles
   
+  
+  def vehicles_by_team
+    vehicles.group_by(&:team)
+  end
+  
+  def vehicles_for_team(team)
+    vehicles_by_team[team]
+  end
+  
   def reversed?
     bookings.first.reversed?
   end
