917f399218db67e917eea6a471b482732946c84b
pezholio <pezholio@gmail.com>
2017-09-25 13:43:29 +0100

    Add form to add a new place

---
+++ app/controllers/admin/places_controller.rb
@@ -0,0 +1,37 @@
+class Admin::PlacesController < AdminController
+  
+  before_filter :get_back_path, :set_defaults
+
+  def new
+  end
+  
+  def create
+    place = Place.create(place_params)
+    if place.valid?
+      redirect_to @back_path
+    else
+      flash[:error] = place.errors.to_a.join('<br/>')
+      render :new
+    end
+  end
+  
+  private
+  
+    def place_params
+      params.require(:place).permit(:name, :latitude, :longitude)
+    end
+    
+    def get_back_path
+      @back_path = if params[:previous_route_id]
+        new_admin_route_stop_path(params[:previous_route_id])
+      else
+        admin_team_path
+      end
+    end
+    
+    def set_defaults
+      @place = Place.new
+      @route_id = params[:previous_route_id]
+    end
+
+end
