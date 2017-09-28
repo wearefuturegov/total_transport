2bf6465774df59ed045477512438eafae21f8653
pezholio <pezholio@gmail.com>
2017-09-28 09:54:09 +0100

    Refactor web steps

--- spec/features/steps/booking_steps.rb
+++ spec/features/steps/booking_steps.rb
@@ -8,12 +8,6 @@ step 'my booking should be a single journey' do
   expect(@booking.return_journey_id).to be_nil
 end
 
-step 'my booking should be confirmed' do
-  expect(Booking.count).to eq(1)
-  @booking = Booking.first
-  expect(@booking.state).to eq('booked')
-end
-
 step 'my booking should be a single journey' do
   expect(@booking.return_journey_id).to be_nil
 end
--- spec/features/steps/web_steps.rb
+++ spec/features/steps/web_steps.rb
@@ -1,96 +1,114 @@
-step 'I visit /journeys' do
-  visit '/journeys'
-end
+module WebSteps
 
-step 'I choose a :text_field point of :point' do |text_field, point|
-  fill_in text_field, with: point
-  wait_for_ajax
-  find('.easy-autocomplete-container li').click
-end
-
-step 'I click See available journeys' do
-  find('#submit').click
-end
-
-step 'I should see :n journey(s)' do |n|
-  expect(page).to have_css('.options form.new_booking', :count => n)
-end
-
-step 'I have chosen a journey' do
-  [
-    'I choose a from point of Newmarket',
-    'I choose a to point of Haverhill',
-    'I click See available journeys'
-  ].each { |s| step s }
-  click_button 'Request one way or return'
-end
-
-step 'I have chosen a journey with :n passengers' do |passengers|
-  [
-    'I choose a from point of Newmarket',
-    'I choose a to point of Haverhill',
-    'I click See available journeys'
-  ].each { |s| step s }
-  send 'I choose :num passengers', passengers
-  click_button 'Request one way or return'
-end
-
-step 'I click on the first journey\'s request button' do
-  click_button 'Request one way or return'
-end
-
-step 'I choose the first return option' do
-  find('#returnTab').click
-  first('label').click
-  click_button 'Next'
-end
-
-step 'I don\'t choose a return journey' do
-  find('#singleTab').click
-  click_button 'Next'
-end
-
-step 'I don\'t add any special requirements' do
-  click_button 'Next'
-end
-
-step 'I choose a pickup and dropoff point' do
-  send 'I choose a :type point of :lat and :lng', 'pickup', @route.stops.first.place.latitude, @route.stops.first.place.longitude
-  send 'I choose a :type point of :lat and :lng', 'dropoff', @route.stops.last.place.latitude, @route.stops.last.place.longitude
-end
-
-step 'I choose a :type point of :lat and :lng' do |type, lat, lng|
-  page.execute_script("$('#booking_#{type}_lat').val('#{lat}')")
-  page.execute_script("$('#booking_#{type}_lng').val('#{lng}')")
-  page.execute_script("$('.disabled').attr('disabled', false)")
-  wait_for_ajax
-  click_button 'Next'
-end
-
-step 'I fill in my details' do
-  fill_in 'booking_passenger_name', with: 'My Name'
-  fill_in 'booking_phone_number', with: '+15005550006'
-  click_button 'Confirm phone number'
-end
-
-step 'I enter my confirmation code' do
-  Passenger.last.verification_code.split('').each_with_index do |num, i|
-    fill_in "digit#{i + 1}", with: num
+  def choose_place(field, place)
+    fill_in field, with: place
+    wait_for_ajax
+    find('.easy-autocomplete-container li').click
   end
-  click_button 'Verify'
-end
 
-step 'I choose :num passengers' do |num|
-  select(num, from: 'booking[number_of_passengers]')
-end
-
-step 'I choose :n child ticket(s)' do |num|
-  num.to_i.times do |n|
-    first('#add-concession').click
-    expect(page).to have_css('#concession-list')
-    select 'Child fare', from: 'concession-list'
+  def click_see_available_journeys
+    find('#submit').click
   end
-  click_button 'Next'
+
+  def click_request_one_way_or_return
+    click_button 'Request one way or return'
+  end
+
+  def choose_passengers(num)
+    select(num, from: 'booking[number_of_passengers]')
+  end
+
+  def choose_first_return_journey
+    find('#returnTab').click
+    first('label').click
+    click_next
+  end
+
+  def choose_single_journey
+    find('#singleTab').click
+    click_next
+  end
+
+  def choose_pickup_dropoff_point(type, lat, lng)
+    page.execute_script("$('#booking_#{type}_lat').val('#{lat}')")
+    page.execute_script("$('#booking_#{type}_lng').val('#{lng}')")
+    page.execute_script("$('.disabled').attr('disabled', false)")
+    wait_for_ajax
+    click_next
+  end
+
+  def fill_in_details(first_name, phone_numer)
+    fill_in 'booking_passenger_name', with: first_name
+    fill_in 'booking_phone_number', with: phone_numer
+    click_button 'Confirm phone number'
+  end
+
+  def click_next
+    click_button 'Next'
+  end
+
+  def enter_confirmation_code(code)
+    code.split('').each_with_index do |num, i|
+      fill_in "digit#{i + 1}", with: num
+    end
+    click_button 'Verify'
+  end
+
+  def enter_first_passengers_confirmation_code
+    enter_confirmation_code(Passenger.last.verification_code)
+  end
+
+  def choose_child_tickets(num)
+    num.to_i.times do |n|
+      first('#add-concession').click
+      expect(page).to have_css('#concession-list')
+      select 'Child fare', from: 'concession-list'
+    end
+    click_button 'Next'
+  end
+
+  step 'I visit /journeys' do
+    visit '/journeys'
+  end
+
+  step :choose_place, 'I choose a :text_field point of :place'
+  step :click_see_available_journeys, 'I click See available journeys'
+  step :choose_passengers, 'I choose :num passengers'
+  step :click_request_one_way_or_return, 'I click on the first journey\'s request button'
+  step :choose_first_return_journey, 'I choose the first return option'
+  step :choose_single_journey, 'I don\'t choose a return journey'
+  step :click_next, 'I don\'t add any special requirements'
+  step :enter_first_passengers_confirmation_code, 'I enter my confirmation code'
+  step :choose_child_tickets, 'I choose :n child ticket(s)'
+
+  step 'I should see :n journey(s)' do |n|
+    expect(page).to have_css('.options form.new_booking', :count => n)
+  end
+
+  step 'I have chosen a journey' do
+    choose_place('from', 'Newmarket')
+    choose_place('to', 'Haverhill')
+    click_see_available_journeys
+    click_request_one_way_or_return
+  end
+
+  step 'I have chosen a journey with :n passengers' do |passengers|
+    choose_place('from', 'Newmarket')
+    choose_place('to', 'Haverhill')
+    click_see_available_journeys
+    choose_passengers(passengers)
+    click_request_one_way_or_return
+  end
+
+  step 'I choose a pickup and dropoff point' do
+    choose_pickup_dropoff_point('pickup', @route.stops.first.place.latitude, @route.stops.first.place.longitude)
+    choose_pickup_dropoff_point('dropoff', @route.stops.last.place.latitude, @route.stops.last.place.longitude)
+  end
+
+  step 'I fill in my details' do
+    fill_in_details('My Name', '+15005550006')
+  end
+
 end
 
 placeholder :text_field do
@@ -103,4 +121,8 @@ placeholder :outward_or_return do
   match /(outward|return)/ do |direction|
     direction == 'return'
   end
+end
+
+RSpec.configure do |config|
+  config.include WebSteps, type: :feature
 end
