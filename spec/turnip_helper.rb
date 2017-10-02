7a2ad1158206fdd17c2f0552c0eb3486a9d3c456
pezholio <pezholio@gmail.com>
2017-10-02 11:36:33 +0100

    Run Chrome headless in CI

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -10,13 +10,17 @@ Capybara.register_driver :selenium do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  # capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-  #   chromeOptions: { args: %w(headless disable-gpu) }
-  # )
+  if ENV['CIRCLECI'] || ENV['HEADLESS']
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w(headless disable-gpu) }
+    )
+  else
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
+  end
 
   Capybara::Selenium::Driver.new app,
-    browser: :chrome#,
-    #desired_capabilities: capabilities
+    browser: :chrome,
+    desired_capabilities: capabilities
 end
 
 Capybara.javascript_driver = :headless_chrome
