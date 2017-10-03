6a9b0ac9686c4144d51e1964c701ea6a096ccb67
Stuart Harrison <pezholio@gmail.com>
2017-10-03 09:39:40 +0100

    Move features to turnip (#32)
    * Setup Turnip
    
    * Move rspec features to Turnip
    
    * Refactor web steps
    
    * Move methods below steps
    
    * Make feature output more cucumber-esque
    
    * Run Chrome headless in CI

---
+++ spec/features/steps/booking_steps.rb
@@ -0,0 +1,21 @@
+step 'my booking should be confirmed' do
+  expect(Booking.count).to eq(1)
+  @booking = Booking.first
+  expect(@booking.state).to eq('booked')
+end
+
+step 'my booking should be a single journey' do
+  expect(@booking.return_journey_id).to be_nil
+end
+
+step 'my booking should be a single journey' do
+  expect(@booking.return_journey_id).to be_nil
+end
+
+step 'my booking should have :n passenger(s)' do |n|
+  expect(@booking.number_of_passengers).to eq(n.to_i)
+end
+
+step 'my booking should have :n child ticket(s)' do |n|
+  expect(@booking.child_tickets).to eq(n.to_i)
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,28 +11,10 @@ require 'timecop'
 
 require 'support/fake_sms'
 require 'support/controller_macros'
-require 'support/wait_for_ajax'
-require 'selenium/webdriver'
 
 SmsService.client = FakeSMS
 
 ActiveRecord::Migration.maintain_test_schema!
-
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu) }
-  )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-end
-
-Capybara.javascript_driver = :headless_chrome
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
@@ -59,16 +41,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :feature) do
-    # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so continue to use transaction strategy for speed.
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    if !driver_shares_db_connection_with_specs
-      # Driver is probably for an external browser with an app
-      # under test that does *not* share a database connection with the
-      # specs, so use truncation strategy.
-      DatabaseCleaner.strategy = :truncation
-    end
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
