22a9b1b83e158f542db9d504689df76bc7057051
pezholio <pezholio@gmail.com>
2017-10-11 18:08:43 +0100

    Remove os_id from place

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,8 +7,9 @@ class Place < ActiveRecord::Base
   
   default_scope { order(name: :asc) }
   
-  validates_presence_of :name, :latitude, :longitude
-  validates_presence_of :os_id, message: 'You must specify a town or village'
+  validates_presence_of :name
+  validates_presence_of :latitude, message: 'You must choose a location'
+  validates_presence_of :longitude, message: 'You must choose a location'
   
   def lat_lng
     Geokit::LatLng.new(self.latitude, self.longitude)
