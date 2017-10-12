c4d4bf6441ca299f7d19d983ea4ac6aded9f0d19
pezholio <pezholio@gmail.com>
2017-10-12 09:47:54 +0100

    Remove redundant code from places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,16 +1,6 @@
 class PlacesController < PublicController
   
   before_filter :get_places, only: [:index]
-  before_filter :get_place, only: [:show]
-  
-  rescue_from ActiveRecord::RecordNotFound do
-    if params[:origin]
-      TrackFailedPlaceQuery.enqueue(params[:origin], params[:name], DateTime.now.to_s)
-    elsif params[:name]
-      TrackFailedPlaceQuery.enqueue(params[:name], nil, DateTime.now.to_s)
-    end
-    head 404, "content_type" => 'text/plain'
-  end
   
   def index
     respond_to do |format|
@@ -28,10 +18,6 @@ class PlacesController < PublicController
     
     def get_places
       @places = places_params[:query].nil? ? Place.all : Place.where("name ILIKE ?", "#{places_params[:query]}%")
-    end
-    
-    def get_place
-      @place = Place.find_by_os_id!(params[:id])
     end
 
 end
--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -13,29 +13,6 @@ RSpec.describe PlacesController, type: :controller, que: true do
     expect(json.count).to eq(5)
   end
   
-  it 'gets a place by its OS ID' do
-    place = FactoryGirl.create(:place, os_id: 12345)
-    get 'show', format: :json, id: 12345
-  
-    json = JSON.parse response.body
-    expect(json['name']).to eq(place.name)
-  end
-  
-  context 'when an OS ID is not present' do
-    
-    let(:id) { 34567 }
-    let(:subject) {
-      get 'show', format: :json, id: id, name: 'Some Place'
-    }
-    let(:datetime) { '2017-01-01T09:00:00+00:00' }
-    
-    before(:each) { Timecop.freeze(datetime) }
-    after(:each) { Timecop.return }
-
-    it 'returns a 404' do
-      subject
-      expect(response.code).to eq('404')
-    end
   it 'searches places' do
     FactoryGirl.create_list(:place, 5)
     
@@ -58,21 +35,6 @@ RSpec.describe PlacesController, type: :controller, que: true do
         
     get 'index', format: :json, query: 'so'
     
-    context 'with an origin' do
-      
-      let(:subject) {
-        get 'show', format: :json, id: id, name: 'Some Place', origin: 'Some other place'
-      }
-      
-      it 'queues up a metrics job' do
-        expect { subject }.to change{
-          QueJob.count
-        }.by(1)
-        expect(QueJob.last.job_class).to eq('TrackFailedPlaceQuery')
-        expect(QueJob.last.args).to eq(['Some other place', 'Some Place', datetime])
-      end
-      
-    end
     json = JSON.parse response.body
     
     expect(json.count).to eq(1)
