d3240f3fee1e63c962c5f50a5e849e3d9752d841
pezholio <pezholio@gmail.com>
2017-10-13 16:46:49 +0100

    Fix munged schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171011160921) do
-=======
-ActiveRecord::Schema.define(version: 20171011140041) do
->>>>>>> Remove polygons from database
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
