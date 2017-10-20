1859fb251d7622de7803095a7f2d6a579f26485d
pezholio <pezholio@gmail.com>
2017-10-20 12:57:18 +0100

    Add booking mailers

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -5,5 +5,20 @@ class BookingMailer < ActionMailer::Base
     @booking = booking
     mail(to: ENV['RIDE_ADMIN_EMAIL'], from: ENV['RIDE_ADMIN_EMAIL'], subject: 'A new booking has been confirmed')
   end
+  
+  def user_confirmation(booking)
+    @booking = booking
+    mail(to: booking.email, from: ENV['RIDE_ADMIN_EMAIL'], subject: 'Your Ride booking confirmation')
+  end
+  
+  def first_alert(booking)
+    @booking = booking
+    mail(to: booking.email, from: ENV['RIDE_ADMIN_EMAIL'], subject: 'Your Ride booking is tomorrow')
+  end
+  
+  def second_alert(booking)
+    @booking = booking
+    mail(to: booking.email, from: ENV['RIDE_ADMIN_EMAIL'], subject: 'Your Ride is on it’s way.')
+  end
 
 end
