e10da6231f2f8c427b51c281e37054158e7875bc
Chris Evans <evanschris@users.noreply.github.com>
2017-11-15 12:21:18 +0000

    add placeholder for bus pass

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,23 +1,23 @@
 module BookingsHelper
-  
+
   def map(lat, lng, width = 1000, height = 175)
     url = generate_url(lat, lng, width, height)
     data = get_data(url)
     data_url = "data:image/png;base64,#{data}"
     image_tag data_url
   end
-  
+
   def bus_passes
     options_for_select([
-      [''],
+      ['Choose a bus pass', ''],
       ['Older Persons Bus Pass', 'older_bus_passes'],
       ['Disabled Bus Pass', 'disabled_bus_passes'],
       ['Scholar Bus Pass', 'scholar_bus_passes']
     ])
   end
-  
+
   private
-  
+
     def generate_url(lat, lng, width, height)
       base = 'https://api.mapbox.com/v4/mapbox.streets'
       geojson = URI.escape "{\"type\":\"Point\",\"coordinates\":[#{lng},#{lat}]}", '"{}[]'
@@ -26,10 +26,10 @@ module BookingsHelper
       access_token = ENV['MAPBOX_ACCESS_TOKEN']
       "#{base}/geojson(#{geojson})/#{centre_and_zoom}/#{size}.png?access_token=#{access_token}"
     end
-    
+
     def get_data(url)
       body = open(url).read
       Base64.encode64(body)
     end
-  
+
 end
