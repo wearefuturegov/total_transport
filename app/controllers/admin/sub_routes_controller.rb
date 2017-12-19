a5902a812a53802adb6ad4f2ff3876fc0e80b443
pezholio <pezholio@gmail.com>
2017-12-19 12:54:48 +0000

    Split controllers up

---
+++ app/controllers/admin/sub_routes_controller.rb
@@ -0,0 +1,33 @@
+class Admin::SubRoutesController < AdminController
+  before_filter :check_permissions
+  before_filter :get_route, only: [:show, :update, :sort, :destroy]
+  
+  def index
+    @routes = Route.where(route_id: params[:route_id])
+    @route = Route.new
+    @back_path = admin_account_path
+    render 'admin/routes/index'
+  end
+
+  def create
+    @route = Route.copy!(params[:route_id])
+    redirect_to admin_route_sub_route_path(params[:route_id], @route)
+  end
+
+  def show
+    @back_path = admin_routes_path
+    render 'admin/routes/show'
+  end
+  
+  def destroy
+    @route.destroy
+    redirect_to admin_routes_path
+  end
+  
+  private
+  
+  def get_route
+    @route = Route.find_by(id: params[:id], route_id: params[:route_id])
+  end
+  
+end
