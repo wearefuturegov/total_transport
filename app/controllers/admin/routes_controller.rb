a9912f6e266edb52edc9d5c93a93f30f39335620
pezholio <pezholio@gmail.com>
2017-12-19 11:18:14 +0000

    Add subroutes to controller

--- app/controllers/admin/routes_controller.rb
+++ app/controllers/admin/routes_controller.rb
@@ -1,34 +1,38 @@
 class Admin::RoutesController < AdminController
   before_filter :check_permissions
+  before_filter :get_route, only: [:show, :update, :sort, :destroy]
   
   def index
-    @routes = Route.all
+    @routes = params[:route_id].nil? ? Route.all : Route.where(route_id: params[:route_id])
     @route = Route.new
     @back_path = admin_account_path
   end
 
   def create
-    route = Route.create!
-    redirect_to admin_route_path(route)
+    @route = params[:route_id].nil? ? Route.create! : Route.create!(route_id: params[:route_id])
+    redirect_to admin_route_path(@route)
   end
 
   def show
-    @route = Route.find(params[:id])
     @back_path = admin_routes_path
   end
   
   def destroy
-    @route = Route.find(params[:id])
     @route.destroy
     redirect_to admin_routes_path
   end
   
   def sort
-    @route = Route.find(params[:id])
     params[:stop].each_with_index do |value,index|
       @route.stops.find(value).update_attribute(:position,index+1)
     end
     render :nothing => true
+  end
+  
+  private
+  
+  def get_route
+    @route = params[:route_id].nil? ? Route.find(params[:id]) : Route.find_by(id: params[:id], route_id: params[:route_id])
   end
   
 end
