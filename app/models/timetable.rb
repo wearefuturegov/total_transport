1562310fa1f34c598a8fc56fd6499172e9205ce3
pezholio <pezholio@gmail.com>
2018-01-23 10:53:37 +0000

    Allow a timetable to create journeys

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,3 +1,35 @@
 class Timetable < ActiveRecord::Base
-  has_many :routes
+  belongs_to :route
+  belongs_to :vehicle
+  belongs_to :supplier
+
+  has_many :journeys
+  
+  after_create :create_journeys
+  
+  private
+  
+    def create_journeys
+      (from..to).each do |date|
+        times.each do |t|
+          t['journeys'] ||= []
+          time = DateTime.parse(t['time']).strftime('%H:%M')
+          start_time = DateTime.parse "#{date.to_s}T#{time}"
+          t['journeys'] << create_journey(start_time).id
+        end
+      end
+      save
+    end
+    
+    def create_journey(start_time)
+      journeys.create(journey_params.merge({start_time: start_time}))
+    end
+    
+    def journey_params
+      {
+        vehicle: vehicle,
+        supplier: supplier,
+        route: route
+      }
+    end
 end
--- spec/models/timetable_spec.rb
+++ spec/models/timetable_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Timetable, type: :model do
+  
+  let(:vehicle) { FactoryBot.create(:vehicle) }
+  let(:supplier) { FactoryBot.create(:supplier) }
+  let(:route) { FactoryBot.create(:route) }
+
+  describe 'create_journeys' do
+    
+    let(:timetable) do
+      FactoryBot.create(:timetable,
+        from: DateTime.now,
+        to: DateTime.now + 3.days,
+        times: [
+          {
+            time: Time.parse('10:00')
+          },
+          {
+            time: Time.parse('14:00')
+          },
+          {
+            time: Time.parse('17:00')
+          }
+        ]
+      )
+    end
+    
+    before { timetable.reload }
+    
+    it 'creates journeys for a given time / date range' do
+      expect(timetable.journeys.count).to eq(12)
+    end
+    
+    it 'adds journey ids to times' do
+      expect(timetable.times[0]['journeys'].count).to eq(4)
+      expect(timetable.times[1]['journeys'].count).to eq(4)
+      expect(timetable.times[2]['journeys'].count).to eq(4)
+      timetable.times.each do |t|
+        t['journeys'].each do |j|
+          time = DateTime.parse(t['time']).strftime('%H:%M')
+          expect(Journey.find(j).start_time.strftime('%H:%M')).to eq(time)
+        end
+      end
+    end
+    
+  end
+  
 end
   
