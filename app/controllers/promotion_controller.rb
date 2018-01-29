0d8a643e36c2319f5b6f431ca5464228e2656144
Martin J Thompson <hiremyskills@martinjthompson.com>
2018-01-29 18:02:04 +0000

    Add promotion basic page with new type

---
+++ app/controllers/promotion_controller.rb
@@ -0,0 +1,12 @@
+class PromotionController < ApplicationController
+  layout 'application'
+  
+  before_filter :create_body_id
+
+  def index
+  end
+
+  def create_body_id
+    @body_id = "#{params[:controller]}-#{params[:action]}"
+  end
+end
