23bc7e7730656bb3e6b32186bb0f1bfe5447951b
pezholio <pezholio@gmail.com>
2018-02-02 14:18:05 +0000

    Tweak borked migration

--- db/migrate/20180131133457_replace_supplier_reference_with_team_in_timetable.rb
+++ db/migrate/20180131133457_replace_supplier_reference_with_team_in_timetable.rb
@@ -2,7 +2,7 @@ class ReplaceSupplierReferenceWithTeamInTimetable < ActiveRecord::Migration
   def change
     add_reference :timetables, :team
     Timetable.all.each do |t|
-      supplier = ActiveRecord::Base.connection.execute("SELECT * FROM suppliers where id=#{j.supplier_id}").first
+      supplier = ActiveRecord::Base.connection.execute("SELECT * FROM suppliers where id=#{t.supplier_id}").first
       t.team_id = supplier['team_id']
       t.save
     end
