426f00a216f6d4df96715fa28430d12313e1df95
Martin J Thompson <hiremyskills@martinjthompson.com>
2018-02-20 20:42:55 +0000

    Add privacy pages and copy

---
+++ app/controllers/privacy_controller.rb
@@ -0,0 +1,16 @@
+class PrivacyController < ApplicationController
+  
+  before_action :set_page_title
+  before_filter :create_body_id
+
+  def index
+  end
+
+  def set_page_title
+    @page_title = t('service')
+  end
+
+  def create_body_id
+    @body_id = "#{params[:controller]}-#{params[:action]}"
+  end
+end
