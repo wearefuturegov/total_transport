4cfb0281a26fec54fc5881418c4b31e0a66f3716
pezholio <pezholio@gmail.com>
2018-07-23 15:45:22 +0100

    Remove raise_in_transactional_callbacks

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,6 @@ module TotalTransport
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/lib)
     
     config.x.pickup.council = ENV.fetch('PICKUP_COUNCIL', 'essex')
