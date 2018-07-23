56e2e3a6321b362157205116e70b5564c608e531
pezholio <pezholio@gmail.com>
2018-07-23 12:54:45 +0100

    Fix bad migration

--- db/migrate/20180426091124_move_personal_data_to_passenger.rb
+++ db/migrate/20180426091124_move_personal_data_to_passenger.rb
@@ -3,9 +3,9 @@ class MovePersonalDataToPassenger < ActiveRecord::Migration
     add_column :passengers, :email, :string
     Booking.all.each do |b|
       passenger = (b.passenger || Passenger.new)
-      passenger.email = b.email
+      passenger.email = b.read_attribute(:email)
       passenger.name = b.passenger_name
-      passenger.phone_number = b.phone_number if passenger.phone_number.nil?
+      passenger.phone_number = b.read_attribute(:phone_number) if passenger.phone_number.nil?
       passenger.save
     end
     remove_column :bookings, :passenger_name, :string
