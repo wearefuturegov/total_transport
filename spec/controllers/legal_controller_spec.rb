f1f4821fe65646dcfa94e3870b96d5dbc8921e68
pezholio <pezholio@gmail.com>
2018-07-23 16:05:12 +0100

    Get rid of deprecation message

--- spec/controllers/legal_controller_spec.rb
+++ spec/controllers/legal_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe LegalController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
     end
   end
 
